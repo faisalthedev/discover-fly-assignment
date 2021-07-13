3ccb4a28e2c38172876dc10a4374f57ee9cc6320
faisalthedev <faisal13108@gmail.com>
2021-07-13 15:00:14 +0600

    Get Ticket Price function added

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ function getTicketQuantity(id) {
 	const ticketConvert = parseInt(ticketQuantity.value);
 	return ticketConvert;
 }
+
+// Get Ticket Price
+function calculateTicketPrice(ticketClass, isIncrease) {
+	const convertQuantity = getTicketQuantity(ticketClass + "-ticket-quantity");
+
+	let totalTicket = convertQuantity;
+
+	if (isIncrease == true) {
+		totalTicket = convertQuantity + 1;
+	} else if (isIncrease == false && convertQuantity > 0) {
+		totalTicket = convertQuantity - 1;
+	}
+
+	document.getElementById(ticketClass + "-ticket-quantity").value = totalTicket;
+	totalCalculation();
+}
